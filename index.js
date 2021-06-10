3474e8ca68fc7d5d7e5cfb3754aea930049eab77
leeilhyung <leeih83@gmail.com>
2021-06-10 16:02:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
   webviewContainer:{
     height:"100%",
     flex:0,
-  }
+  },
   loadingOverlayContainer: { position: "absolute", top: 0, bottom: 0, left: 0, right: 0, alignItems: "center", justifyContent: "center" },
 });
 
